87dbe65badc59b8d8c0e815ce13181d88e4b747f
VelVelikov <webeltek12@gmail.com>
2025-06-29 11:04:06 +0200

    Modify comment to test push

--- Horizons/Program.cs
+++ Horizons/Program.cs
@@ -12,7 +12,7 @@ namespace Horizons
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container. Modif comment
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
